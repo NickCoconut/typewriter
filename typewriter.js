28a247926b26de3603227dabac5c915db8824657
NickCoconut <gelaxyy@gmail.com>
2021-11-30 22:34:21 +0000

    add typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,15 @@
+const sentence = "hello there from lighthouse labs";
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+      return (sentence[i] === sentence.length - 1 ? 
+      process.stdout.write(sentence[i] + '\n')
+    :
+    process.stdout.write(sentence[i]))    
+}, 50 * i)
+
+
+
+    
+};
+
+
